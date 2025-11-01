a6941c3c5eb0a508f93e8b6e9e208bae82389c13
kimdaegwon <daegwonkim.dev@gmail.com>
2025-11-01 17:25:40 +0900

    first commit

---
+++ src/main/java/io/github/daegwonkim/event_coupon_issuance_system/EventCouponIssuanceSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.daegwonkim.event_coupon_issuance_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCouponIssuanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCouponIssuanceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/daegwonkim/event_coupon_issuance_system/EventCouponIssuanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.daegwonkim.event_coupon_issuance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCouponIssuanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
