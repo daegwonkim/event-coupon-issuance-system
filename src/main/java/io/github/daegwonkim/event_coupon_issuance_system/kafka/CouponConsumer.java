357daee845c0aea7070fc6a910c404b22013a3c7
kimdaegwon <daegwonkim.dev@gmail.com>
2025-11-03 14:13:16 +0900

    refactor: 예외 처리 조건 추가

--- src/main/java/io/github/daegwonkim/event_coupon_issuance_system/kafka/CouponConsumer.java
+++ src/main/java/io/github/daegwonkim/event_coupon_issuance_system/kafka/CouponConsumer.java
@@ -5,6 +5,7 @@ import io.github.daegwonkim.event_coupon_issuance_system.service.v8.CouponServic
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -38,6 +39,9 @@ public class CouponConsumer {
 
                 log.info("쿠폰 발급 완료 - userId: {}, couponId: {}",
                         request.userId(), request.couponId());
+            } catch (DataIntegrityViolationException e) {
+                log.warn("쿠폰 중복 발급 감지 - userId: {}, couponId: {}, error: {}",
+                        request.userId(), request.couponId(), e.getMessage());
             } catch (Exception e) {
                 log.error("쿠폰 발급 실패 - userId: {}, couponId: {}, error: {}",
                         request.userId(), request.couponId(), e.getMessage());
